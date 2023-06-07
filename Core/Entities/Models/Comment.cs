6a8fe27b051fb629e52c844df5da987c02057349
jaugustyn <kub4a1@gmail.com>
2023-06-07 21:07:12 +0200

    feat: create models

---
+++ Core/Entities/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.Models;
+
+public record Comment : IEntityBase
+{
+    public string? Text { get; init; }
+    public Guid ReviewId { get; init; }
+    public Guid UserId { get; init; }
+    public DateTimeOffset CreatedDate { get; init; }
+    public Guid Id { get; init; }
+}
---
+++ Core/Entities/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.Models;
+
+public record Review : IEntityBase
+{
+    public string Title { get; init; }
+    public string Text { get; init; }
+    public Guid UserId { get; init; }
+    public DateTimeOffset CreatedDate { get; init; }
+    public Guid Id { get; init; }
+}
