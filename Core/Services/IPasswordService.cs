2d864bdd7caae3154379297b9ff65b0937fdb6e3
jaugustyn <kub4a1@gmail.com>
2023-06-07 21:11:00 +0200

    fix: add password service

---
+++ Core/Services/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Services;
+
+public interface IPasswordService
+{
+    string HashPassword(string password, out byte[] salt);
+    bool VerifyPassword(string password, string hash, byte[] salt);
+}
