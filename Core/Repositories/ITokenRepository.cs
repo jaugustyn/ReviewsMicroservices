022688505b04070b88cec942fdb3bfebaf90c867
jaugustyn <kub4a1@gmail.com>
2023-06-07 21:10:19 +0200

    feat: add tokenrepository

---
+++ Core/Repositories/ITokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Models;
+
+namespace Core.Repositories;
+
+public interface ITokenRepository
+{
+    Task<RefreshToken> GetByIdAsync(Guid id);
+    Task<RefreshToken> GetByTokenAsync(string token);
+    Task<RefreshToken> GetByUserIdAsync(Guid id);
+    Task<RefreshToken> AddTokenAsync(RefreshToken token);
+    Task UpdateTokenAsync(RefreshToken token);
+}
