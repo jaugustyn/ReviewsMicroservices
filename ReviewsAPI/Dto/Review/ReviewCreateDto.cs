a10baefc2fe8eda1369d21985d3ad5fc1f635691
jaugustyn <kub4a1@gmail.com>
2023-06-07 21:13:57 +0200

    feat: add review DTOs

---
+++ ReviewsAPI/Dto/Review/ReviewCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ReviewsAPI.Dto.Review;
+
+public class ReviewCreateDto
+{
+    public string Title { get; init; }
+    public string Text { get; init; }
+}
---
+++ ReviewsAPI/Dto/Review/ReviewUpdateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ReviewsAPI.Dto.Review;
+
+public class ReviewUpdateDto
+{
+    public string Title { get; init; }
+    public string Text { get; init; }
+}
