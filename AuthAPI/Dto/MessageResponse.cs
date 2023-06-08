4dcb7f445a61175c6e2a7ea98b2d6b5c32d5263b
jaugustyn <kub4a1@gmail.com>
2023-06-08 23:56:54 +0200

    feat: add reponse messages model

---
+++ AuthAPI/Dto/MessageResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthAPI.Dto;
+
+public class MessageResponse
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Message { get; set; } = null;
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? ErrorMessage { get; set; } = null;
+
+    public MessageResponse(string? message = null, string? errorMessage = null)
+    {
+        Message = message;
+        ErrorMessage = errorMessage;
+    }
+}
