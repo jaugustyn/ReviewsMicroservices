25c35f9131263db365079966cd21c7bff58376cc
jaugustyn <kub4a1@gmail.com>
2023-06-13 01:58:36 +0200

    fix: make sure bson serializers are registered once

--- Infrastructure/Configuration/ConfigureMongoDb.cs
+++ Infrastructure/Configuration/ConfigureMongoDb.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 
 namespace Infrastructure.Configuration;
 
@@ -14,41 +13,40 @@ public static class ConfigureMongoDb
 {
     public static void ConfigureMongo(this IServiceCollection services, IConfiguration configuration)
     {
-        // https://stackoverflow.com/questions/51270502/how-to-configure-bsonserializer-without-use-of-static-registry
-        // Not thread safe, btw doesnt work...
-        // if(BsonSerializer.LookupSerializer<Guid>().GetType() != typeof(GuidSerializer))
-        // {
-        //     BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-        //     BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-        // }
+        BsonSerializerRegisterer.RegisterSerializers();
 
-        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-        
         services.AddHealthChecks().AddCheck<MongoHealthCheck>("MongoDbConnectionCheck");
-        
+
         services.Configure<MongoDbSettings>(configuration.GetSection(nameof(MongoDbSettings)));
         
-        var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-        
-        services.AddSingleton<IMongoDbSettings>(serviceProvider => serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
-        services.AddSingleton<IMongoClient>(serviceProvider => new MongoClient(mongoDbSettings.ConnectionString));
-        
+        services.AddSingleton<IMongoDbSettings>(serviceProvider =>
+            serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+        services.AddSingleton<MongoDbContext>();
+
         services.AddControllers().AddJsonOptions(options =>
         {
             // serialize enums as strings in api responses (e.g. Role)
             options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         });
     }
-    public class CustomSerializationProvider : IBsonSerializationProvider
-    {
-        public IBsonSerializer GetSerializer(Type type)
-        {
-            if (type == typeof(Guid)) return new GuidSerializer(BsonType.String);
-            if (type == typeof(Guid)) return new DateTimeOffsetSerializer(BsonType.String);
+}
 
-            // fall back to Mongo's defaults
-            return null;
+public static class BsonSerializerRegisterer
+{
+    static BsonSerializerRegisterer()
+    {
+        try
+        {
+            BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(typeof(DateTimeOffset), new DateTimeOffsetSerializer(BsonType.String));
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error initializing BsonSerializerRegisterer: {ex.Message}");
+        }
+    }
+
+    public static void RegisterSerializers()
+    {
     }
 }
