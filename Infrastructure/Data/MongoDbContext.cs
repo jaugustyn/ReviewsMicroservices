84913b931fd5be4b1c0a2dbb9923c8ed348cb3f6
jaugustyn <kub4a1@gmail.com>
2023-06-13 01:57:54 +0200

    fix: removed useless constructor param

--- Infrastructure/Data/MongoDbContext.cs
+++ Infrastructure/Data/MongoDbContext.cs
@@ -1,8 +1,5 @@
 ﻿using Core.Entities.Models;
 using Infrastructure.Configuration;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Options;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Infrastructure.Data;
@@ -11,14 +8,14 @@ public class MongoDbContext : IMongoDbContext
 {
     private IMongoDatabase Database { get; set; }
     private MongoClient MongoClient { get; set; }
-    
+
     private const string ReviewCollectionName = "Reviews";
     private const string RatingCollectionName = "Ratings";
     private const string CommentCollectionName = "Comments";
     private const string UserCollectionName = "Users";
     private const string RefreshTokenCollectionName = "RefreshTokens";
 
-    public MongoDbContext(IMongoClient mongoClient, IMongoDbSettings mongoDbSettings)
+    public MongoDbContext(IMongoDbSettings mongoDbSettings)
     {
         MongoClient = new MongoClient(mongoDbSettings.ConnectionString);
         Database = MongoClient.GetDatabase(mongoDbSettings.DatabaseName);
@@ -35,6 +32,7 @@ public class MongoDbContext : IMongoDbContext
     public IMongoCollection<Comment> Comments { get; }
     public IMongoCollection<User> Users { get; }
     public IMongoCollection<RefreshToken> RefreshTokens { get; }
+
     public IMongoCollection<T> GetCollection<T>(string name)
     {
         return Database.GetCollection<T>(name);
