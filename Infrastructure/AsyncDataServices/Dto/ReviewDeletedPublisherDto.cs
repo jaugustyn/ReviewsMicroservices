26a6073b902d05e9f1a42d6a947166f886ee4b12
jaugustyn <kub4a1@gmail.com>
2023-06-16 21:24:59 +0200

    fix: added missing parameterless contructor

--- Infrastructure/AsyncDataServices/Dto/ReviewDeletedPublisherDto.cs
+++ Infrastructure/AsyncDataServices/Dto/ReviewDeletedPublisherDto.cs
@@ -5,6 +5,7 @@ public class ReviewDeletedPublisherDto
     public Guid ReviewId { get; set; }
     public string Event { get; set; }
 
+    public ReviewDeletedPublisherDto(){ }
     public ReviewDeletedPublisherDto(Guid reviewId, string eventName)
     {
         ReviewId = reviewId;
--- Infrastructure/AsyncDataServices/Dto/UserDeletedPublisherDto.cs
+++ Infrastructure/AsyncDataServices/Dto/UserDeletedPublisherDto.cs
@@ -5,6 +5,7 @@ public class UserDeletedPublisherDto
     public Guid UserId { get; set; }
     public string Event { get; set; }
 
+    public UserDeletedPublisherDto() {}
     public UserDeletedPublisherDto(Guid userId, string eventName)
     {
         UserId = userId;
